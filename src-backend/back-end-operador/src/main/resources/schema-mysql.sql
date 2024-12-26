0a132a6f436467dfb054bf00de0de12073f87d31
Juan <aguil17@gmail.com>
2024-12-25 22:05:27 -0500

    change schema

--- src-backend/back-end-operador/src/main/resources/schema-mysql.sql
+++ src-backend/back-end-operador/src/main/resources/schema-mysql.sql
@@ -1,3 +1,15 @@
+drop table if exists informant;
+
+create table informant
+(
+    id int AUTO_INCREMENT primary key,
+    name varchar(100) not null,
+    last_name varchar(100) not null,
+    cellphone varchar(60) not null,
+    email varchar(60) not null,
+    creation_date DATETIME not null
+);
+
 
 drop table if exists location;
 
@@ -11,7 +23,7 @@ create table location
     latitude varchar(100) not null,
     longitude varchar(100) not null,
     creation_date DATETIME not null
-)
+);
 
 
 drop table if exists incident;
@@ -31,5 +43,31 @@ create table incident
 alter table incident
 add constraint fk_location
 foreign key (id_location)
-references location(id)
+references location(id);
+
+
+drop table if exists incident_informant;
+
+create table incident_informant
+(
+    id_incident int,
+    id_informat int
+);
+
+alter table incident_informant
+add primary key (id_incident,id_informat);
+
+
+alter table incident_informant
+add constraint fk_incident
+foreign key (id_incident)
+references incident(id);
+
+
+alter table incident_informant
+add constraint fk_informat
+foreign key (id_informat)
+references informant(id);
+
+
 
