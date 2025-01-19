6f757097360d39bbf98b1fbcb16fc8469bed8f67
Juan <aguil17@gmail.com>
2025-01-19 14:40:44 -0500

    Add unit tests

--- src-backend/back-end-operador/src/test/java/com/unir/operador/service/IncidenteServiceTests.java
+++ src-backend/back-end-operador/src/test/java/com/unir/operador/service/IncidenteServiceTests.java
@@ -1,7 +1,12 @@
 package com.unir.operador.service;
 
 import com.unir.operador.data.IIncidenteRepository;
+import com.unir.operador.data.IUbicacionRepository;
+import com.unir.operador.facade.IncidentesFacade;
 import com.unir.operador.model.pojo.Incidente;
+import com.unir.operador.model.pojo.Ubicacion;
+import com.unir.operador.model.request.CreateIncidenteRequest;
+import com.unir.operador.model.request.CreateLocationRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,6 +15,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,12 +27,74 @@ public class IncidenteServiceTests {
     @Mock
     private IIncidenteRepository incidenteRepository;
 
+    @Mock
+    private IUbicacionRepository ubicacionRepository;
+
+    @Mock
+    private IncidentesFacade incidentesFacade;
+
     @InjectMocks
     private IncidenteServiceImpl incidenteService;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this); // Inicializa los mocks
+    }
+
+    @Test
+    public void testCrearIncidente_success()
+    {
+        //arrange
+        CreateIncidenteRequest request = new CreateIncidenteRequest();
+        request.setDescripcion("Fuego en av. principal");
+        request.setTipoIncidente("fire");
+        request.setFecha("2020-10-12");
+        request.setHora("10:30");
+
+        var ubicacion = new CreateLocationRequest();
+        ubicacion.setDescripcion("Fuego en av. principal");
+        ubicacion.setNombreCiudad("Lima");
+        ubicacion.setNombreDistrito("Lima");
+        ubicacion.setReferencia("Esquina Avenida Pardo y Aliaga");
+        ubicacion.setLatitud("-12.046374");
+        ubicacion.setLongitud("-77.042793");
+
+        request.setUbicacion(ubicacion);
+
+        var ubicacionResponse = new Ubicacion();
+        ubicacionResponse.setDescription("Fuego en av. principal");
+        ubicacionResponse.setCity_name("Lima");
+        ubicacionResponse.setDistrict_name("Lima");
+        ubicacionResponse.setReference("Esquina Avenida Pardo y Aliaga");
+        ubicacionResponse.setLatitude("-12.046374");
+        ubicacionResponse.setLongitude("-77.042793");
+
+        when(ubicacionRepository.save(any())).thenReturn(ubicacionResponse);
+
+        var fecha = LocalDate.parse("2020-10-12");
+        var hora = LocalTime.parse("10:30");
+
+        var incidente = new Incidente();
+        incidente.setId(1);
+        incidente.setDescription("Fuego en av. principal");
+        incidente.setDate(fecha);
+        incidente.setTime(hora);
+
+        when(incidenteRepository.save(any())).thenReturn(incidente);
+
+        //act
+        var result = incidenteService.crearIncidente(request);
+
+        //assert
+        verify(ubicacionRepository,times(1)).save(any());
+        verify(incidenteRepository,times(1)).save(any());
+        assertNotNull(result);
+        assertFalse(result.isError());
+        assertEquals("201",result.getCode());
+        assertEquals(incidente.getId(),result.getData().getId());
+        assertEquals(incidente.getDescription(),result.getData().getDescription());
+        assertEquals(incidente.getDate(),result.getData().getDate());
+        assertEquals(incidente.getTime(),result.getData().getTime());
     }
 
     @Test
@@ -59,6 +128,7 @@ public class IncidenteServiceTests {
         verify(incidenteRepository,times(1)).save(any(Incidente.class));
         assertFalse(response.isError());
         assertEquals("200",response.getCode());
+        assertEquals(incidente.getId(),response.getData().getId());
     }
 }
 
