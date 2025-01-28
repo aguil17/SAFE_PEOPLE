26d32410bad68b34d66d6619701a73206639e668
Juan <aguil17@gmail.com>
2025-01-27 20:22:16 -0500

    Add create user logic

--- src-backend/back-end-operador/src/main/java/com/unir/operador/service/IncidenteService.java
+++ src-backend/back-end-operador/src/main/java/com/unir/operador/service/IncidenteService.java
@@ -1,10 +1,8 @@
 package com.unir.operador.service;
 
 import com.unir.operador.model.request.CreateIncidenteRequest;
-import com.unir.operador.model.request.UpdateIncidenteRequest;
 import com.unir.operador.model.response.CreateIncidenteResponse;
 import com.unir.operador.model.response.DeleteIncidenteResponse;
-import com.unir.operador.model.response.UpdateIncidenteResponse;
 
 public interface IncidenteService {
 
