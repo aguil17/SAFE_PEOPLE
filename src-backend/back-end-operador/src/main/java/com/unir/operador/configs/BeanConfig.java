67bae3c1353b28a53ccdda63f83341e634df5aea
Juan <aguil17@gmail.com>
2025-01-29 21:08:36 -0500

    Add create user logic

--- src-backend/back-end-operador/src/main/java/com/unir/operador/configs/BeanConfig.java
+++ src-backend/back-end-operador/src/main/java/com/unir/operador/configs/BeanConfig.java
@@ -1,10 +1,8 @@
 package com.unir.operador.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.unir.operador.util.ExcludeFromCoverage;
 import org.springframework.context.annotation.Bean;
 
-@ExcludeFromCoverage
 public class BeanConfig {
 
     @Bean
